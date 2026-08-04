1a4a0dadfa155efa0512348b4342f13a9bd01b91
SHAIK MAHAMMAD ASHRAF <94215709+ashuoooo@users.noreply.github.com>
2026-08-05 04:44:38 +0530

    Add: task-scheduling - submission-0

---
+++ Data Structures & Algorithms/task-scheduling/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] freq=new int[26];
+        for(char task:tasks){
+            freq[task-'A']++;
+        }
+        int maxfreq=0;
+        for(int f:freq){
+            maxfreq=Math.max(maxfreq,f);
+        }
+        int maxfreqcount=0;
+        for(int f:freq){
+            if(f==maxfreq){
+                maxfreqcount++;
+            }
+        }
+        int calculatedSlots = (maxfreq - 1) * (n + 1) + maxfreqcount;
+
+        return Math.max(tasks.length, calculatedSlots);
+
+    }
+}
